2286951eff85401692653206a5796961bf9a6cae
Mark McClean <mcclean_mark_02@hotmail.com>
2025-01-22 23:43:54 +0100

    added call to action buttons and logo

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRoute } from 'vue-router'
+
+const route = useRoute()
 
 const mobileMenuVisible = ref(false)
 
 function toggleMenu() {
-  console.log('start function')
+  console.log(route.name)
   mobileMenuVisible.value = !mobileMenuVisible.value
   console.log('end function')
 }
@@ -13,13 +15,37 @@ function toggleMenu() {
 
 <template>
   <div class="fixed w-full h-16 flex justify-between items-center p-4 bg-transparent text-white">
-    <div>BRIGHT MOTION</div>
-    <div class="hidden md:block">
+    <div class="flex items-center p-2">
+      <img src="../assets/images/logo4.png" alt="" class="h-12 w-12 mr-4" />
+      <img src="../assets/images/name2.png" alt="" class="w-3/5 sm:w-2/5 md:1/5" />
+      <!-- <p>BRIGHT MOTION</p> -->
+    </div>
+    <div class="hidden md:block mr-4">
       <ul class="flex space-x-8">
-        <li><RouterLink to="/">Home</RouterLink></li>
-        <li><RouterLink to="/about">About</RouterLink></li>
-        <li><RouterLink to="/projects">Projects</RouterLink></li>
-        <li><RouterLink to="/contact">Contact</RouterLink></li>
+        <li
+          class="hover:border-b hover:border-teal-500 hover:text-teal-400"
+          :class="{ 'border-b border-teal-500 text-teal-400': route.name === 'home' }"
+        >
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li
+          class="hover:border-b hover:border-teal-500 hover:text-teal-400"
+          :class="{ 'border-b border-teal-500 text-teal-400': route.name === 'about' }"
+        >
+          <RouterLink to="/about">About</RouterLink>
+        </li>
+        <li
+          class="hover:border-b hover:border-teal-500 hover:text-teal-400"
+          :class="{ 'border-b border-teal-500 text-teal-400': route.name === 'projects' }"
+        >
+          <RouterLink to="/projects">Projects</RouterLink>
+        </li>
+        <li
+          class="hover:border-b hover:border-teal-500 hover:text-teal-400"
+          :class="{ 'border-b border-teal-500 text-teal-400': route.name === 'contact' }"
+        >
+          <RouterLink to="/contact">Contact</RouterLink>
+        </li>
       </ul>
     </div>
     <div class="block md:hidden scale-50 z-50" @click="toggleMenu">
@@ -33,14 +59,40 @@ function toggleMenu() {
       <!-- </label> -->
     </div>
     <div
-      class="h-screen w-screen bg-black absolute top-0 left-0 -translate-y-full transform-gpu duration-300 flex justify-center items-center"
+      class="h-screen w-screen bg-black absolute top-0 left-0 -translate-y-full transform-gpu duration-300 flex flex-col justify-center items-center"
       :class="{ 'translate-y-0': mobileMenuVisible }"
     >
+      <div class="absolute left-1/2 transform -translate-x-1/2 top-20 w-60">
+        <div class="flex flex-col items-center">
+          <img src="../assets/images/logo4.png" alt="" class="h-16 w-16" />
+          <img src="../assets/images/name2.png" alt="" class="" />
+        </div>
+      </div>
       <ul class="space-y-8 text-center">
-        <li><RouterLink @click="toggleMenu" to="/">Home</RouterLink></li>
-        <li><RouterLink @click="toggleMenu" to="/about">About</RouterLink></li>
-        <li><RouterLink @click="toggleMenu" to="/projects">Projects</RouterLink></li>
-        <li><RouterLink @click="toggleMenu" to="/contact">Contact</RouterLink></li>
+        <li
+          class="hover:border-b hover:border-teal-500 hover:text-teal-400"
+          :class="{ 'border-b border-teal-500 text-teal-400': route.name === 'home' }"
+        >
+          <RouterLink @click="toggleMenu" to="/">Home</RouterLink>
+        </li>
+        <li
+          class="hover:border-b hover:border-teal-500 hover:text-teal-400"
+          :class="{ 'border-b border-teal-500 text-teal-400': route.name === 'about' }"
+        >
+          <RouterLink @click="toggleMenu" to="/about">About</RouterLink>
+        </li>
+        <li
+          class="hover:border-b hover:border-teal-500 hover:text-teal-400"
+          :class="{ 'border-b border-teal-500 text-teal-400': route.name === 'projects' }"
+        >
+          <RouterLink @click="toggleMenu" to="/projects">Projects</RouterLink>
+        </li>
+        <li
+          class="hover:border-b hover:border-teal-500 hover:text-teal-400"
+          :class="{ 'border-b border-teal-500 text-teal-400': route.name === 'contact' }"
+        >
+          <RouterLink @click="toggleMenu" to="/contact">Contact</RouterLink>
+        </li>
       </ul>
     </div>
   </div>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -9,11 +9,28 @@ import TheWelcome from '../components/TheWelcome.vue'
       <img src="../assets/images/landing-image.png" alt="" class="h-screen w-screen object-cover" />
       <div class="absolute h-screen w-screen bg-black opacity-50 top-0 left-0 h-auto"></div>
       <div
-        class="absolute flex inset-0 items-center justify-center text-2xl lg:text-5xl text-white"
+        class="absolute flex inset-0 items-center justify-center md:justify-start md:ml-[25%] text-white"
       >
-        <p class="max-w-60 lg:max-w-2xl">
-          Driving sustainable development with international expertise and innovation.
-        </p>
+        <div>
+          <p class="max-w-60 md:max-w-xl lg:max-w-2xl text-3xl md:text-3xl lg:text-5xl mb-6">
+            Driving sustainability
+          </p>
+          <p class="max-w-52 md:max-w-md text-2xl lg:text-3xl">
+            Developed with international expertise and innovation
+          </p>
+          <div class="flex flex-col md:flex-row mt-6">
+            <button
+              class="h-12 w-auto px-8 bg-teal-500 rounded hover:bg-white hover:text-teal-500 mr-8 mb-4 md:mb-0"
+            >
+              Find out more
+            </button>
+            <button
+              class="h-12 w-auto px-8 hover:bg-teal-500 rounded bg-white text-teal-500 hover:text-white"
+            >
+              Contact us
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
