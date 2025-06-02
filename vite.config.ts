967c4a3044034cccb82accfc8f3afe0dd0b2df11
Mark McClean <mcclean_mark_02@hotmail.com>
2025-06-02 22:22:16 +0200

    removed vite plugin prerenderer

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import vueDevTools from 'vite-plugin-vue-devtools'
-import prerender from 'vite-plugin-prerender'
+// import prerender from 'vite-plugin-prerender'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/bright-motion-ventures/',
   plugins: [
     vue(),
-    prerender({
-      staticDir: 'dist',
-      routes: ['/', '/about', '/projects', '/contact'],
-    }),
+    // prerender({
+    //   staticDir: 'dist',
+    //   routes: ['/', '/about', '/projects', '/contact'],
+    // }),
   ],
   // vueDevTools()
   resolve: {
