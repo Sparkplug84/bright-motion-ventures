cadae72ae497853fd467a3928d5514e67d3af027
Mark McClean <mcclean_mark_02@hotmail.com>
2025-06-24 10:41:39 +0200

    rebuild with correct base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bright-motion-ventures/',
+  base: '/',
   plugins: [
     vue(),
     // prerender({
